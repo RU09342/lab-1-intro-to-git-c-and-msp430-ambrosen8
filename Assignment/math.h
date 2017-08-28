ce10bfbe6217c94abdfc9e385e5d882e40bf088a
Russty32280 <traffo17@students.rowan.edu>
2017-08-28 02:41:56 -0400

    Copy code from main repository

---
+++ Assignment/math.h
@@ -0,0 +1,13 @@
+/*
+ * math.h
+ *
+ *  Created on: Aug 22, 2017
+ *      Author: Russty
+ */
+
+#ifndef MATH_H_
+#define MATH_H_
+
+int math(int num1, int num2, char Operator);
+
+#endif /* MATH_H_ */
